eb9eba92e48fff3b832682bb0107f613bedf9656
anhhuy <phamanhhuy22@gmail.com>
2023-07-09 23:44:47 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["dcfnhvdfddskhgvgllox.supabase.co"],
+		domains: ["tjvhqeunrsnhcfydfhxh.supabase.co"],
 	},
 };
 
